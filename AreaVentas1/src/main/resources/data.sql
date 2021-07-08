65cead8860b7755d95202e60ed9be25a0bf8268b
laura-gc <85907919+laura-gc@users.noreply.github.com>
2021-07-08 11:15:59 -0500

    Add files via upload

---
+++ AreaVentas1/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Almacen(nombre, capacidad) VALUES ('Almacen1', '50');
+INSERT INTO Almacen(nombre, capacidad) VALUES ('Almacen2', '100');
+INSERT INTO Almacen(nombre, capacidad) VALUES ('Almacen3', '200');
+
+
+INSERT INTO Kardex(idAlmacen, TipoMov,idDocumento) VALUES (1, 'Salida',1020);
+INSERT INTO Kardex(idAlmacen, TipoMov,idDocumento) VALUES (2, 'Entrada',1021);
+INSERT INTO Kardex(idAlmacen, TipoMov,idDocumento) VALUES (3, 'Salida',1022);
+
