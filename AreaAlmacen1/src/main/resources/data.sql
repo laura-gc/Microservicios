f3c975c445c3ab085c8017fa83d05f2f21428538
laura-gc <85907919+laura-gc@users.noreply.github.com>
2021-07-08 13:00:42 -0500

    Update data.sql

--- AreaAlmacen1/src/main/resources/data.sql
+++ AreaAlmacen1/src/main/resources/data.sql
@@ -1,4 +1,2 @@
 
-INSERT INTO empleado(idEmpleado,Nombres,DocIdentidad,Telefono,Correo,Direccion,FechaRegistro) VALUES (1,'Carolina','123568','9987456','caro@gmail.com','av.Sol','2021-02-02');
 INSERT INTO cliente(idCliente, Nombres,DocIdentidad,Telefono,Correo,Direccion,FechaRegistro) VALUES (1,'Laura','123456789','789654123','laura@gmail.com','Av.sol','2014-02-02');
-INSERT INTO docVenta(idDocVenta,idCliente,idEmpleado,fecha) VALUES (1,1,1,'2021-02-02');
