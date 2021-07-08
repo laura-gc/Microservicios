a2d2117b184b87d88fb0b4d1892ede8a465c0819
laura-gc <85907919+laura-gc@users.noreply.github.com>
2021-07-08 11:29:36 -0500

    Update data.sql

--- AreaVentas1/src/main/resources/data.sql
+++ AreaVentas1/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO Almacen(nombre, capacidad) VALUES ('Almacen1', '50');
-INSERT INTO Almacen(nombre, capacidad) VALUES ('Almacen2', '100');
-INSERT INTO Almacen(nombre, capacidad) VALUES ('Almacen3', '200');
+INSERT INTO Almacen(idAlmacen,nombre, capacidad) VALUES (1,'Almacen1', '50');
+INSERT INTO Almacen(idAlmacen,nombre, capacidad) VALUES (2,'Almacen2', '100');
+INSERT INTO Almacen(idAlmacen,nombre, capacidad) VALUES (3,'Almacen3', '200');
 
 
-INSERT INTO Kardex(idAlmacen, TipoMov,idDocumento) VALUES (1, 'Salida',1020);
-INSERT INTO Kardex(idAlmacen, TipoMov,idDocumento) VALUES (2, 'Entrada',1021);
-INSERT INTO Kardex(idAlmacen, TipoMov,idDocumento) VALUES (3, 'Salida',1022);
+INSERT INTO Kardex(idKardex,idAlmacen, TipoMov,idDocumento) VALUES (1,1, 'Salida',1020);
+INSERT INTO Kardex(idKardex,idAlmacen, TipoMov,idDocumento) VALUES (2,2, 'Entrada',1021);
+INSERT INTO Kardex(idKardex,idAlmacen, TipoMov,idDocumento) VALUES (3,3, 'Salida',1022);
 
