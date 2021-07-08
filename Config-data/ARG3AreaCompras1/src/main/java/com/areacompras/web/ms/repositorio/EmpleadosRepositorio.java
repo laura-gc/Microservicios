d8495de1d1f45ada9e238c67ddaf779c3dc350d0
laura-gc <85907919+laura-gc@users.noreply.github.com>
2021-07-08 13:39:03 -0500

    Delete Config-data/ARG3AreaCompras1 directory

--- Config-data/ARG3AreaCompras1/src/main/java/com/areacompras/web/ms/repositorio/EmpleadosRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.areacompras.web.ms.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.areacompras.web.ms.modelo.Empleados;
-
-public interface EmpleadosRepositorio extends JpaRepository<Empleados, Integer> {
-
-}
--- Config-data/ARG3AreaCompras1/src/main/java/com/areacompras/web/ms/repositorio/ProveedoresRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.areacompras.web.ms.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.areacompras.web.ms.modelo.Proveedores;
-
-public interface ProveedoresRepositorio extends JpaRepository<Proveedores, Integer>{
-
-}
