5c93bb7b012dea2b89fa657dfa0ecadb4540ca82
laura-gc <85907919+laura-gc@users.noreply.github.com>
2021-07-08 13:04:31 -0500

    Update data.sql

--- AreaAlmacen1/src/main/resources/data.sql
+++ AreaAlmacen1/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 
-INSERT INTO cliente(idCliente, Nombres,DocIdentidad,Telefono,Correo,Direccion,FechaRegistro) VALUES (1,'Laura','123456789','789654123','laura@gmail.com','Av.sol','2014-02-02');
+INSERT INTO cliente(idCliente, Nombre,DocIdentidad,Telefono,Correo,Direccion,FechaRegistro) VALUES (2,'Laura','123456789','789654123','laura@gmail.com','Av.sol','2014-02-02')
