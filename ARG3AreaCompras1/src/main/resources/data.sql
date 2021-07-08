bd09e6e4f66d11a9f550c3757927326bb4caac8f
laura-gc <85907919+laura-gc@users.noreply.github.com>
2021-07-08 13:50:34 -0500

    Add files via upload

---
+++ ARG3AreaCompras1/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO empleado (id_empleado, correo, direccion, doc_identidad, fecha_registro, nombres, telefono) VALUES(1,'Caro@gmail.com','Av.sol','12349999','2021-02-02','Carolina','79966547')
+
