95288cc133031e1fc1368a97b22044544bd4d2c3
laura-gc <85907919+laura-gc@users.noreply.github.com>
2021-07-08 12:43:02 -0500

    Add files via upload

---
+++ AreaAlmacen1/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO Clientes(idCliente, Nombres,DocIdentidad,Telefono,Correo,Direccion,FechaRegistro) VALUES (1,'Laura','123456789','789654123','laura@gmail.com','Av.sol','2014-02-02');
+INSERT INTO DocVenta(idDocVenta,idCliente,idEmpleado,fecha) VALUES (1,1,,1,'2021-02-02');
