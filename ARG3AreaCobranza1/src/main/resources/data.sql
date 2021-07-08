5dcc048874bd4641fcebe88ae11e6314c90a7af2
laura-gc <85907919+laura-gc@users.noreply.github.com>
2021-07-08 14:30:31 -0500

    Add files via upload

---
+++ ARG3AreaCobranza1/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO empleados (id_empleado, correo, direccion, doc_identidad, fecha_registro, nombres, telefono) VALUES(1,'Caro@gmail.com','Av.sol','12349999','2021-02-02','Carolina','79966547')
